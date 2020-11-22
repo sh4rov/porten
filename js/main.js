79024c5b046446ca5e345462f896784b017f180b
kek <sh4rov@gmail.com>
2020-11-23 00:51:25 +0300

    add breakpoint slider

--- js/main.js
+++ js/main.js
@@ -1,4 +1,32 @@
-const mySwiper = new Swiper('.swiper-container', {
-  slidesPerView: 3,
+const swiperParams = {
+  slidesPerView: 1,
   loop: true,
-})
+
+  // autoplay: {
+  //   delay: 3000,
+  // },
+
+  breakpoints: {
+    540: {
+      slidesPerView: 2,
+      loop: true,
+    },
+
+    700: {
+      slidesPerView: 2,
+      loop: true,
+    },
+
+    768: {
+      slidesPerView: 3,
+      loop: true,
+    },
+
+    1200: {
+      slidesPerView: 3,
+      loop: true,
+    }
+  }
+}
+
+const mySwiper = new Swiper('.swiper-container', swiperParams)
